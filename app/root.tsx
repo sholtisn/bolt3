fa0d4ca449cf07bc1ba1b043e0583e1f805610e0
sholtisn <nsholtis2304@gmail.com>
2025-05-15 15:32:25 -0400

    Fix Tailwind CSS configuration

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
 import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
 import {PageLayout} from './components/PageLayout';
-import tailwindCss from './styles/tailwind.css';
+import styles from './styles/tailwind.css';
 
 export type RootLoader = typeof loader;
 
@@ -45,7 +45,7 @@ export function links() {
       href: 'https://shop.app',
     },
     {rel: 'icon', type: 'image/svg+xml', href: favicon},
-    {rel: 'stylesheet', href: tailwindCss},
+    {rel: 'stylesheet', href: styles},
     {rel: 'stylesheet', href: resetStyles},
     {rel: 'stylesheet', href: appStyles},
   ];
